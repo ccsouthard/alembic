983e55739216a1a8cded17eeb4d5da54aa6dd1a6
Ali <fastian.star@gmail.com>
2013-05-16 10:10:52 +0500

    GuestBook page template.

---
+++ template_guestbook.php
@@ -0,0 +1,22 @@
+<?php /* Template Name: GuestBook */ ?>
+<section>
+	<?php s_breadcrumbs(); ?>
+	<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+		<div class="entry">
+			<?php if(!get_the_title()==''){echo '<h2>'.get_the_title().'</h2>';}?>
+			<?php the_content(); ?>
+			<div id="comments">
+<?php comments_template(); ?>
+</div>
+		</div>
+		
+	<?php endwhile; ?>
+	
+	<?php else: //If no posts are present ?>
+		<div class="entry">
+			<p><?php _e('No posts were found.', 's'); ?></p>
+		</div>
+	<?php endif; ?>
+</section>
+
+<?php include("wrapper.php"); ?>
