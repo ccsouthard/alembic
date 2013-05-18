9788879ceddb932449cecca247c2d6070b6641f1
Sean Lang <slang800@gmail.com>
2013-05-18 17:21:40 -0500

    added a semantic clear fix & applied it to all pages closes #44

--- page.php
+++ page.php
@@ -1,16 +1,13 @@
-<section>
-	<?php s_breadcrumbs(); ?>
-	<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-		<div class="entry">
-			<?php if(!get_the_title()==''){echo '<h2>'.get_the_title().'</h2>';}?>
-			<?php the_content(); ?>
-		</div>
-	<?php endwhile; ?>
-	<?php else: //If no posts are present ?>
-		<div class="entry">
-			<p><?php _e('No posts were found.', 's'); ?></p>
-		</div>
-	<?php endif; ?>
-</section>
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+	<section class="entry group">
+		<?php if(!get_the_title()==''){echo '<h2>'.get_the_title().'</h2>';}?>
+		<?php the_content(); ?>
+	</section>
+<?php endwhile; ?>
+<?php else: //If no posts are present ?>
+	<section class="entry group">
+		<p><?php _e('No posts were found.', 's'); ?></p>
+	</section>
+<?php endif; ?>
 
 <?php include("wrapper.php"); ?>
