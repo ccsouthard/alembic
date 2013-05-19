35bcb6fc5e3c1b186d8f1b58adba2425bd1236eb
Sean Lang <slang800@gmail.com>
2013-05-18 19:11:35 -0500

    refactored based off of home template

--- single-portfolio.php
+++ single-portfolio.php
@@ -1,13 +1,14 @@
-<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-	<?php the_content(); ?>
-<?php endwhile; endif; ?>
-<div class="grid-100">
-	<?php 
-		$promobox = get_post_meta($post->ID,'_promo_area',true);
-		echo $promobox; 
+<?php /* Template Name: Single Portfolio */ ?>
+<section>
+	<?php
+	if(have_posts()){
+		while(have_posts()){
+			the_post();
+			the_content();
+		}
+	}
 	?>
-</div>
-<div class="clear"></div>
+</section>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/assets/css/gallery.css">
 
 <?php include("wrapper.php"); ?>
