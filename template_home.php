dc19f7e3c11457feb0d0761c92c956d7b503d678
Sean Lang <slang800@gmail.com>
2013-05-18 19:07:29 -0500

    refactored home

--- template_home.php
+++ template_home.php
@@ -1,17 +1,13 @@
 <?php /* Template Name: Home */ ?>
 <section>
-		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-				<?php
-					if(!get_the_title()=='')
-						echo '<h2>'.get_the_title().'</h2>';
-					the_content();
-				?>
-		<?php endwhile; ?>
-		<?php else: ?>
-			<div class="entry">
-				<p><?php _e('No posts were found.', 's'); ?></p>
-			</div>
-		<?php endif; ?>
+	<?php
+	if(have_posts()){
+		while(have_posts()){
+			the_post();
+			the_content();
+		}
+	}
+	?>
 </section>
 
 <?php include("wrapper.php"); ?>
