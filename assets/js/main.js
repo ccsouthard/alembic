c8eab3db86180ad79ff76b2ef1c6a939000df7ff
Sean Lang <slang800@gmail.com>
2013-05-25 15:24:58 -0500

    added guestbook thing in footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
-jQuery(function(){
-
+jQuery(document).ready(function ($) {
 	// initialise plugins
-	jQuery("#tabs").tabs();
+	$("#tabs").tabs();
 
-	jQuery('ul.columns li:last-child').addClass('last');
+	$('ul.columns li:last-child').addClass('last');
 
 	//portfolio style
-	jQuery('.data').hover(
+	$('.data').hover(
 		function(){
-			link = jQuery(this).find('.con');
+			link = $(this).find('.con');
 			link.animate({top: '50%', opacity: '1'},200);
 		},
 		function(){
@@ -17,21 +16,21 @@ jQuery(function(){
 	);
 
 	// hide #back-top first
-	jQuery("#back-top").hide();
+	$("#back-top").hide();
 
 	// fade in #back-top
-	jQuery(function () {
-		jQuery(window).scroll(function () {
-			if (jQuery(this).scrollTop() > 100) {
-				jQuery('#back-top').fadeIn();
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#back-top').fadeIn();
 			} else {
-				jQuery('#back-top').fadeOut();
+				$('#back-top').fadeOut();
 			}
 		});
 
 		// scroll body to 0px on click
-		jQuery('#back-top a').click(function () {
-			jQuery('body,html').animate({
+		$('#back-top a').click(function () {
+			$('body,html').animate({
 				scrollTop: 0
 			}, 800);
 			return false;
@@ -39,18 +38,55 @@ jQuery(function(){
 	});
 
 	//function to stay menu top when scroll down
-	var menu = jQuery('#main-nav'),
+	var menu = $('#main-nav'),
 	pos = menu.offset();
 
-	jQuery(window).scroll(function(){
-		if(jQuery(this).scrollTop() > pos.top+menu.height() && menu.hasClass('default')){
+	$(window).scroll(function(){
+		if($(this).scrollTop() > pos.top+menu.height() && menu.hasClass('default')){
 			menu.fadeOut('fast', function(){
-				jQuery(this).removeClass('default').addClass('fixed').fadeIn('fast');
+				$(this).removeClass('default').addClass('fixed').fadeIn('fast');
 			});
-		} else if(jQuery(this).scrollTop() <= pos.top && menu.hasClass('fixed')){
+		} else if($(this).scrollTop() <= pos.top && menu.hasClass('fixed')){
 			menu.fadeOut('fast', function(){
-				jQuery(this).removeClass('fixed').addClass('default').fadeIn('fast');
+				$(this).removeClass('fixed').addClass('default').fadeIn('fast');
 			});
 		}
 	});
+
+
+	//Cookie Handling Functions
+	function bakeCookie(name, value) {
+		var expires = new Date();
+		expires.setTime(expires.getTime() + (15552000000));
+		if(value === ''){//set cookie, or if value is blank, set it to be removed
+			document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+		} else {
+			document.cookie = name + "=" + value + "; expires=" + expires.toGMTString() + "; path=/";
+		}
+	}
+	function eatCookie(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		var cookieValue = "";
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') c = c.substring(1);
+			if (c.indexOf(nameEQ) === 0) {
+				cookieValue = c.substring(nameEQ.length);
+				break;
+			}
+		}
+		return cookieValue;
+	}
+	$('#continue-to-guestbook').click(function(){
+		bakeCookie('name', $('#guestbook-name').val());
+		bakeCookie('email', $('#guestbook-email').val());
+		window.location = '/guestbook';
+	});
+	console.log(window.location.pathname);
+	if(window.location.pathname == '/guestbook/'){
+		$('#comment_name').val(eatCookie('name'));
+		$('#comment_email').val(eatCookie('email'));
+	}
+
 });
--- footer.php
+++ footer.php
@@ -1,19 +1,26 @@
 <footer>
 	<div class="wrap">
-		<?php
-			//Include file for the columns
-			include(S_INCLUDES . 'widget-columns.php');
-		?>
-		<p class="copy">
-			<?php
-				$default_copyright = '&copy; <a href="' . get_bloginfo('url') . '">' . get_bloginfo('name') . '</a> ' . date('Y');
-				echo stripslashes(get_option('s_footer_copyright', $default_copyright)); 
-			?>
-		</p>
+		<div id="sign_guestbook">
+			<p>Sign Our Guestbook</p>
+			<span class="wpcf7-form-control-wrap" style="padding-left:0">
+				<input id='guestbook-name' type="text" value="Your Name"/>
+			</span>
+			<span class="wpcf7-form-control-wrap">
+				<input id='guestbook-email' type="text" value="Email Address"/>
+			</span>
+			<span class="wpcf7-form-control-wrap">
+				<button id="continue-to-guestbook">Continue...</button>
+			</span>
+		</div>
 		<p id="back-top">
 			<a href="#top">
 				<span></span>
 			</a>
+		</p>
+		<p class="copy">
+			<?php
+				echo '&copy;' . date('Y') . ' - <a href="http://ccsdesignhouse.com">CCS</a>';
+			?>
 		</p>
 	</div>
 	<?php wp_footer(); ?>
